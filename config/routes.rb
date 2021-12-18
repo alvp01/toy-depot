301e7499fe49bd55e958ac8192187e65d7e7f8cd
Abel Lavieri <abel.lavieri@gmail.com>
2021-12-17 20:40:39 -0400

    Add store controller and style to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get 'store/index'
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'products#index'
+  root to: 'store#index'
 end
